798394e2d652e66bab9295e8e1843f3d193b6f35
zekai <zekai@zipcodes-MacBook-Pro.local>
2020-11-15 18:55:03 -0500

    Added TestUser, updated tests

--- src/test/java/ATM/TestAccount.java
+++ src/test/java/ATM/TestAccount.java
@@ -6,43 +6,72 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestAccount {
-    Account account = new Account();
+    Account account = new Account(10.00);
 
     @Test
     public void testDeposit() {
-        account.balance = 0.0;
-        account.Deposit(10.00);
-        Double expectedAmt = 10.00;
+        Double amt = 10.00;
+        Double expectedAmt = account.checkBalance()+amt;
 
-        assertEquals(expectedAmt, account.balance);
+        account.Deposit(amt);
+        Double actualAmt = account.checkBalance();
+
+        assertEquals(expectedAmt, actualAmt);
     }
 
     @Test
     public void testWithdraw() {
-        account.balance = 20.00;
-        account.withdraw(15.00);
-        Double expectedAmt = 5.00;
+        Double amt = 5.00;
+        Double expectedAmt = account.checkBalance()-amt;
 
-        assertEquals(expectedAmt, account.balance);
+        account.withdraw(amt);
+        Double actualAmt = account.checkBalance();
+
+        assertEquals(expectedAmt, actualAmt);
     }
 
     @Test
     public void testBalance() {
-        account.balance = 20.00;
-        Double expectedBalance = 20.00;
-        assertEquals(expectedBalance, account.checkBalance());
+        Double expectedBalance = 10.00;
+        Double actualBalance = account.checkBalance();
+        assertEquals(expectedBalance, actualBalance);
     }
 
     @Test
     public void testIsEmpty(){
-        account.balance = 0.00;
+        Double amt = 10.00;
+        account.withdraw(10.00);
         assertTrue(account.checkIfEmpty());
     }
 
     @Test
     public void testIsEmpty2(){
-        account.balance = 1.00;
         assertFalse(account.checkIfEmpty());
     }
+
+    @Test
+    public void printTransTest1(){
+        Double amt = 10.00;
+        account.withdraw(10.00);
+        account.Deposit(1.00);
+
+        Integer expectedX = 2;
+        Integer actualX = account.transHistory.size();
+
+        assertEquals(expectedX,actualX);
+    }
+
+    @Test
+    public void printTransTest2(){
+        Double amt = 10.00;
+        account.withdraw(10.00);
+        account.Deposit(1.00);
+
+        Integer expectedX = 3;
+        Integer actualX = account.transHistory.size();
+
+        assertNotEquals(expectedX,actualX);
+    }
+
 
 }
